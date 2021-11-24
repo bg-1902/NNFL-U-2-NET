97106aee1af33644f2a65626d52d4cf4c0583a54
bg-1902 <f20190181@pilani.bits-pilani.ac.in>
2021-11-24 22:41:05 +0530

    Update u2net_train.py

--- u2net_train.py
+++ u2net_train.py
@@ -59,7 +59,7 @@ label_ext = '.png'
 model_dir = os.path.join('/content/gdrive/MyDrive/G22/', 'saved_models', model_name + os.sep)
 
 
-epoch_num = 20
+epoch_num = 1000
 batch_size_train = 22
 batch_size_val = 1
 train_num = 0
